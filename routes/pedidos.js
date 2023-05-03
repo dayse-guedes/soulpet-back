f9ade94620eba094fe25a371902c529c87b432d8
Fernanda-Franco <fernandaf.vet@gmail.com>
2023-05-03 15:35:34 -0300

    Implementação da Rota Put em Pedidos

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -20,6 +20,31 @@ try {
   }
 });
 
+router.put("/pedidos/:codigo", async (req, res, next) => {
+  const { clienteId, produtoId, quantidade } = req.body;
+  const { codigo } = req.params;
+
+  if(
+    !clienteId ||
+    !produtoId || 
+    !quantidade
+  ) {
+    return res.status(400).json({ message: "Campo obrigatório não informado" });
+  }
+  try {
+    const pedido = await Pedido.findOne({ where: { codigo } });
+    if (pedido) {
+        await pedido.update({ clienteId, produtoId, quantidade });
+      res.status(200).json({ message: "Pedido atualizado." });
+    } else {
+      res.status(404).json({ message: "Pedido não encontrado." });
+    }
+  } catch (err) {
+    console.error(err);
+    next(err)
+  }
+});
+
 
 
 module.exports = router;
