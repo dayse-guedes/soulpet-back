93f73cd4f8adf2ca4a14956d79eb48227ac4c80d
thomazinig <guilherme_thomazini245@hotmail.com>
2023-05-03 09:25:00 -0300

    adicionado post para inserção de pedidos

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -5,5 +5,21 @@ const Pedido = require("../database/pedido");
 
 const router = Router();
 
+router.post("/pedidos", async (req, res) => {
+const {quantidade,clienteId,produtoId} = req.body;
+try {
+    
+    const novo = await Pedido.create(
+      { quantidade,clienteId,produtoId}
+    );
+
+    res.status(201).json(novo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Um erro aconteceu." });
+  }
+});
+
+
 
 module.exports = router;
