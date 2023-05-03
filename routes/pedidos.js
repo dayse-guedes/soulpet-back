304e94f7ac4e5917ce330323cbbd8ef4dd542418
thomazinig <guilherme_thomazini245@hotmail.com>
2023-05-03 13:38:16 -0300

    adicinado validação

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -8,14 +8,22 @@ const router = Router();
 router.post("/pedidos", async (req, res) => {
 const {quantidade,clienteId,produtoId} = req.body;
 try {
+    if (quantidade && clienteId && produtoId) {
+      
     
     const novo = await Pedido.create(
       { quantidade,clienteId,produtoId}
     );
 
     res.status(201).json(novo);
-  } catch (err) {
+  }else{
+    res.status(400).json("dados invalidos")
+  }
+
+  }
+   catch (err) {
     console.log(err);
+
     res.status(500).json({ message: "Um erro aconteceu." });
   }
 });
